09f3a41a3c85cf18a6e233f38819876cc1a31b68
zhengwujun <zhengwujun@bytedance.com>
2022-08-03 10:12:22 +0800

    update

--- src/demo_selfHook.jsx
+++ src/demo_selfHook.jsx
@@ -1,4 +1,4 @@
-import React , { useState, useMemo, useRef } from 'react'
+import React, { useState, useMemo, useRef } from 'react'
 /* 自定义hooks 用于格式化数组将小写转成大写 */
 function useFormatList(list) {
   return useMemo(() => list.map(item => {
@@ -13,8 +13,8 @@ function useFormatList(list) {
 
 function Wrapper() {
   let [count, setCount] = useState(0)
-  let [list, setList] = useState([ 'aaa' , 'bbb' , 'ccc' ])
-  let list1 = useRef([ '111' , '222' , '333' ]).current
+  let [list, setList] = useState(['aaa', 'bbb', 'ccc'])
+  let list1 = useRef(['111', '222', '333']).current
   // let list1 = [ '111' , '222' , '333' ]
   return (
     <>
@@ -22,15 +22,15 @@ function Wrapper() {
         <span>count: {count}</span> <button onClick={() => setCount(count + 1)}>加count</button>
       </div>
       <hr />
-      <button onClick={() => {setList([...list, 'ddd'])}}>改list</button>
+      <button onClick={() => { setList([...list, 'ddd']) }}>改list</button>
       <ul>
         {
-          useFormatList(list).map((item, index)=><div key={index} >{ item }</div>)
+          useFormatList(list).map((item, index) => <div key={index} >{item}</div>)
         }
       </ul>
       <ul>
         {
-          useFormatList(list1).map((item, index)=><div key={index} >{ item }</div>)
+          useFormatList(list1).map((item, index) => <div key={index} >{item}</div>)
         }
       </ul>
     </>
